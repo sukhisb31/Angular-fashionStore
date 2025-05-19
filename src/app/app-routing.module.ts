34a48720555c1c1e6e23b5d1cf8fc49e23f3da17
[Sukhwinder Singh] <[sukhisb31@gmail.coml]>
2025-05-19 13:15:29 +0530

    till login fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './myComponents/home/home.component';
 import { SignupComponent } from './myComponents/form/signup/signup.component';
+import { LoginComponent } from './myComponents/form/login/login.component';
+import { ProductListComponent } from './myComponents/product-list/product-list.component';
 
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
+  {path:'login', component:LoginComponent},
   {path:'signup', component:SignupComponent},
+  {path:'productList', component:ProductListComponent},
   {path: '', component:HomeComponent, pathMatch: 'full'},
 
 ];
