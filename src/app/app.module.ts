c1d259aa1feecdf8f54f437581b3a766a7a77a16
[Sukhwinder Singh] <[sukhisb31@gmail.coml]>
2025-05-19 17:59:58 +0530

    till footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './myComponents/navbar/navbar.component';
 import { HomeComponent } from './myComponents/home/home.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { ProductListComponent } from './myComponents/product-list/product-list.component';
 import { LoginComponent } from './myComponents/form/login/login.component';
 import { SignupComponent } from './myComponents/form/signup/signup.component';
-
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,12 +39,13 @@ import { SignupComponent } from './myComponents/form/signup/signup.component';
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-   MatFormFieldModule,
-   MatCardModule,
-  MatInputModule,
-  ReactiveFormsModule
-
-
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
